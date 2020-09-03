9206e69a907475eefc3dacf752270364113c7f98
aidanclarkescott <clarkescotta@gmail.com>
2020-09-03 10:07:16 -0700

    Added score feature

--- FlappyBirdProject/Assets/Scripts/Level.cs
+++ FlappyBirdProject/Assets/Scripts/Level.cs
@@ -9,8 +9,15 @@ public class Level : MonoBehaviour {
     private const float PIPE_MOVE_SPEED = 30f;
     private const float PIPE_DESTROY_X_POSITION = -100f;
     private const float PIPE_SPAWN_X_POSITION = 100f;
+    private const float BIRD_X_POSITION = 0f;
+
+    private static Level instance;
+    public static Level GetInstance() {
+        return instance;
+    }
 
     private List<Pipe> pipeList;
+    private int pipesPassedCount;
     private int pipesSpawned;
     private float pipeSpawnTimer;
     private float pipeSpawnTimerMax;
@@ -24,6 +31,7 @@ public class Level : MonoBehaviour {
     }
 
     private void Awake() {
+        instance = this;
         pipeList = new List<Pipe>();
         pipeSpawnTimerMax = 1f;
         SetDifficulty(Difficulty.Easy);
@@ -56,7 +64,12 @@ public class Level : MonoBehaviour {
     private void HandlePipeMovement() {
         for (int i = 0; i < pipeList.Count; i++) {
             Pipe pipe = pipeList[i];
+            bool isToTheRightOfBird = pipe.GetXPosition() > BIRD_X_POSITION;
             pipe.Move();
+
+            if (isToTheRightOfBird && pipe.GetXPosition() <= BIRD_X_POSITION && pipe.IsBottom()) {
+                pipesPassedCount++;
+            }
 
             if (pipe.GetXPosition() < PIPE_DESTROY_X_POSITION) {
                 pipe.DestroySelf();
@@ -131,17 +144,27 @@ public class Level : MonoBehaviour {
         pipeBodyBoxCollider.size = new Vector2(PIPE_WIDTH, height);
         pipeBodyBoxCollider.offset = new Vector2(0f, height * 0.5f);
 
-        Pipe pipe = new Pipe(pipeHead, pipeBody);
+        Pipe pipe = new Pipe(pipeHead, pipeBody, createBottom);
         pipeList.Add(pipe);
+    }
+
+    public int GetPipesSpawned() {
+        return pipesSpawned;
+    }
+
+    public int GetPipesPassedCount() {
+        return pipesPassedCount;
     }
 
     private class Pipe {
         private Transform pipeHeadTransform;
         private Transform pipeBodyTransform;
+        private bool isBottom;
 
-        public Pipe(Transform head, Transform body) {
+        public Pipe(Transform head, Transform body, bool isBottom) {
             this.pipeHeadTransform = head;
             this.pipeBodyTransform = body;
+            this.isBottom = isBottom;
         }
 
         public void Move() {
@@ -151,6 +174,10 @@ public class Level : MonoBehaviour {
 
         public float GetXPosition() {
             return pipeHeadTransform.position.x;
+        }
+
+        public bool IsBottom() {
+            return isBottom;
         }
 
         public void DestroySelf() {
