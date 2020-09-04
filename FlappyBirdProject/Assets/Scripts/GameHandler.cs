4e7751f0f4a477f8b5da1d8e862d335d2af12a2e
aidanclarkescott <clarkescotta@gmail.com>
2020-09-03 19:07:46 -0700

    Version 1.1 Final Build (fixed highscore saves)

--- FlappyBirdProject/Assets/Scripts/GameHandler.cs
+++ FlappyBirdProject/Assets/Scripts/GameHandler.cs
@@ -6,7 +6,15 @@ using CodeMonkey.Utils;
 
 public class GameHandler : MonoBehaviour {
     // Start is called before the first frame update
+
+    private static bool firstTime = true;
+
     void Start() {
+        if (firstTime) {
+            Score.ResetHighscore();
+            firstTime = false;
+        }
+
         Score.Start();
     }
 }
